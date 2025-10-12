daf49a9343f77520f87cc561498afa558de4c1ec
Adhim Niokagi <niokagi@gmail.com>
2025-10-12 08:40:45 +0700

    create order service template code

---
+++ src/services/orders.service.ts
@@ -0,0 +1,9 @@
+import apiClient from "@/lib/axios";
+// orders payloaf
+// orders interface
+interface OrdersResponse {
+    status: string,
+    message: string,
+    data: string[]
+}
+// code functions here
